5dd9aa25919be19cb01dd5003a75fa39d0a37761
Shuntoria Reid <shuntoriareid240@gmail.com>
2024-09-10 13:46:23 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [message, setMessage] = useState('');
+    const [aiResponse, setAiResponse] = useState('');
+
+    useEffect(() => {
+        fetch('/')
+            .then(response => response.text())
+            .then(data => setMessage(data));
+    }, []);
+
+    const getAiResponse = () => {
+        fetch('/ai-tutor')
+            .then(response => response.json())
+            .then(data => setAiResponse(data));
+    };
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>{message}</p>
+                <button onClick={getAiResponse}>Get AI Response</button>
+                <p>{aiResponse}</p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
